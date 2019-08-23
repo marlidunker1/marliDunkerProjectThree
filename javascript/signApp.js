21e0da6a190d7d207e76242a3de22357ad6370b6
Marli Dunker <marlidunker@gmail.com>
2019-08-23 09:03:37 -0400

    live typing start

--- javascript/signApp.js
+++ javascript/signApp.js
@@ -4,14 +4,34 @@ signApp.userSign = () => {
         e.preventDefault();
 
         const userInput = $('input').val();
+
+        // $(userInput).bind('keydown keypress', () => {
+        //     setTimeout(() => {
+        //         $('h1').text(input.val());
+        //     }, 0.5);
+        // });
         
         if (userInput !== '') {
             $('h1').html(`<h1>${userInput}</h1>`);
             $('input').val('');
-            $("form.sizeButtons").css("visibility", "visible");            
+            $("form.sizeButtons").css("visibility", "visible"); 
+            $('h1').css('color', 'rgba(0, 0, 0, 1)');          
         } else {
             //NEED TO ERROR HANDLE HERE
         }
+    });
+}
+
+signApp.biggerSmaller = () => {
+    $('form.sizeButtons').on('submit', (e) => {
+        e.preventDefault();
+        
+    });
+    $('#smaller').on('click', () => {
+        $('h1').css('font-size','-=5');
+    });
+    $('#bigger').on('click', () => {
+        $('h1').css('font-size','+=5');
     });
 }
 
@@ -20,5 +40,6 @@ signApp.userSign = () => {
 
 $(document).ready( () => {
     signApp.userSign();
+    signApp.biggerSmaller();
 });
 
