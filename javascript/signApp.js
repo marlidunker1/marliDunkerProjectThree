0d4415fd253c3deee6675390be9fcdd9980bce2e
Marli Dunker <marlidunker@gmail.com>
2019-08-23 13:04:24 -0400

    before lunch commmit

--- javascript/signApp.js
+++ javascript/signApp.js
@@ -1,44 +1,47 @@
 const signApp = {};
+signApp.signType = () => {
+    $('input#sign').on('keyup', (e) => {
+        let userInput = $('input').val();
+            
+        if (e.keyCode !== 13) {
+            $('h1').text(`${userInput}`);
+        }
+    });
+    // $('input#sign').focus(); //LOOKING TO FOCUS //ALSO SHIFT PROBLEM
+}
+
 signApp.userSign = () => {
     $('form.sign').on('submit', (e) => {
         e.preventDefault();
 
-        const userInput = $('input').val();
+        let userInput = $('input').val();
 
-        // $(userInput).bind('keydown keypress', () => {
-        //     setTimeout(() => {
-        //         $('h1').text(input.val());
-        //     }, 0.5);
-        // });
-        
         if (userInput !== '') {
-            $('h1').html(`<h1>${userInput}</h1>`);
             $('input').val('');
             $("form.sizeButtons").css("visibility", "visible"); 
             $('h1').css('color', 'rgba(0, 0, 0, 1)');          
-        } else {
-            //NEED TO ERROR HANDLE HERE
+        } else if (userInput === '') {
+            $('h1').text("Type field above is required").css('color', 'red');
         }
     });
 }
 
 signApp.biggerSmaller = () => {
-    $('form.sizeButtons').on('submit', (e) => {
+    $('#smaller').on('click', (e) => {
         e.preventDefault();
-        
-    });
-    $('#smaller').on('click', () => {
         $('h1').css('font-size','-=5');
     });
-    $('#bigger').on('click', () => {
+    $('#bigger').on('click', (e) => {
+        e.preventDefault();
         $('h1').css('font-size','+=5');
+    });
+    $('#reset').on('click', () => {
+        //THIS IS RUNNING THE DEFAULT SO THE PAGE RESETS WHEN THE BUTTON IS CLICKED
     });
 }
 
-
-
-
 $(document).ready( () => {
+    signApp.signType();
     signApp.userSign();
     signApp.biggerSmaller();
 });
